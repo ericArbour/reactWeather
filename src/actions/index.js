5f2d0f91644b976e815db45bb144c4d6309ed8b8
Eric Arbour <arboureric@gmail.com>
2018-01-31 22:12:50 -0500

    finished video 64

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '5bafcdb08c58b0fa8107acc75ede8045';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
